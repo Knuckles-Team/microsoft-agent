ab7a086980ae0502ff1ca829e6fb470d589a4d06
knucklessg1 <knucklessg1@gmail.com>
2026-05-18 02:21:01 -0500

    Bump version: 0.11.0 → 0.12.0

--- microsoft_agent/mcp_server.py
+++ microsoft_agent/mcp_server.py
@@ -30,7 +30,7 @@ from starlette.responses import JSONResponse
 
 from microsoft_agent.auth import get_client
 
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 
 logger = get_logger(name="microsoft-agent")
 logger.setLevel(logging.INFO)
